d212bac2861325eecb9fd52cdbee2c08d0a2e187
scraph <raphael.uzan.aa@gmail.com>
2016-12-01 22:18:17 +0100

    Init composant architecture

---
+++ frontend/source/FicheAchat.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Col } from 'react-bootstrap'
+
+const FicheAchat = (props) => {
+
+  return (
+    <div>
+      <Col md='6'>
+        <Button> Click me </Button>
+      </Col>
+
+
+
+    </div>
+  )
+
+}
+
+export default FicheAchat
