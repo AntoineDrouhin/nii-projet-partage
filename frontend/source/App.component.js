61dd7c8ada4cddca26f26e18c899dbc279bbd144
jonasmaz <jonasmaz@hotmail.fr>
2016-12-02 01:04:34 +0100

    Formulaire inscription

--- frontend/source/App.component.js
+++ frontend/source/App.component.js
@@ -2,15 +2,17 @@ import React from 'react';
 import HelloWorld from './HelloWorld.component.js'
 import FicheAchat from './FicheAchat.component.js'
 import {Col, Row} from 'react-bootstrap'
+import Inscription from './Inscription.component.js'
 
 const App = (props) => {
 
   return (
     <div>
 
-      <Col md="6" style={{backgroundColor: "lightblue"}}>
-        
-          <FicheAchat {...props} />
+      <Col md={6} style={{backgroundColor: "lightblue"}}>
+
+          <Inscription {...props}/>
+          
 
 
       </Col>
