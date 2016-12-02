57fc2297283960f383770a7476bc5aa428bfef65
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-12-02 04:32:09 +0100

    Remove HelloWorld

--- frontend/source/components/App.component.js
+++ frontend/source/components/App.component.js
@@ -1,26 +1,21 @@
 import React from 'react';
 
 import FicheAchat from './FicheAchat.component.js'
-import Profil from './Profil.component.js'
+import Profil from '../container/Profil.container.js'
 import FormFicheAchat from './FormFicheAchat.component.js'
 import Inscription from './Inscription.component.js'
 
-import {Col, Row} from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 import Inscription from './Inscription.component.js'
 
 const App = (props) => {
-
   return (
     <div>
-
-      <Col md={6} style={{backgroundColor: "lightblue"}}>
-
-          <Inscription {...props}/>
-
+      <Col md={3} style={{backgroundColor: "lightblue"}}>
+          <Profil {...props}/>
       </Col>
     </div>
   )
-
 }
 
 export default App
--- frontend/source/components/FicheAchat.component.js
+++ frontend/source/components/FicheAchat.component.js
@@ -7,12 +7,12 @@ const FicheAchat = (props) => {
     <Panel>
 
       <Col sm={3} md={6}>
-      <h1>[Fiche Article x]</h1>
-      <h2>Nom Produit :</h2>
-      <h2>Nom Demandeur :</h2>
-      <h2>Montant :</h2>
-      <Button bsStyle="success">+</Button>
-              <h1></h1>
+        <h1>[Fiche Article x]</h1>
+        <h2>Nom Produit :</h2>
+        <h2>Nom Demandeur :</h2>
+        <h2>Montant :</h2>
+        <Button bsStyle="success">+</Button>
+        <h1></h1>
       </Col>
 
     </Panel>
